8df8adb7c62be3279a5b10067b76df7dba18077d
Oliver Flyckt Wilhjelm <olfw@itu.dk>
2022-09-09 14:55:13 +0200

    regex wip

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,5 +1,45 @@
+using FluentAssertions;
+using Xunit;
+
 namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+
+    [Fact]
+    public void TestSplitLine()
+    {
+        //Assign
+        string[] test = {"text test string", "animal dog cat", "math 22+23-result"};
+    
+        //Act
+        var check = RegExpr.SplitLine(test);
+    
+        //Assert
+        check.Should().BeEquivalentTo(new [] {"text", "test", "string", "animal", "dog", "cat", "math", "22", "23", "result"});
+    }
+
+    [Fact]
+    public void TestResolutions()
+    {
+        //Assign
+        string[] test = {"1920x1080", "720x1080", "4000x2000", "10000x3"};
+    
+        //Act
+        var check = RegExpr.Resolution(test);
+    
+        //Assert
+        check.Should().BeEquivalentTo(new[] {(1920, 1080),(720, 1080),(4000, 2000),(100000, 3)});
+    }
+
+    [Fact]
+    public void TestInnerText()
+    {
+        //Assign
+        string[] test = {""};
+    
+        //Act
+    
+        //Assert
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,31 @@
+using System.Text.RegularExpressions;
+
 namespace Assignment1;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines){
+        var pattern = @"([a-zA-Z0-9]+)";
+        foreach(string s in lines){
+            foreach(Match m in Regex.Matches(s, pattern)){
+                yield return m.Value;
+            }
+           }
+        }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions){
+        var pattern = @"(?<width>[0-9]+)x(?<height>[0-9]+)";
+        foreach(string s in resolutions){
+            var match = Regex.Match(s, pattern);
+            if(match.Success){
+                 Console.WriteLine((int.Parse(match.Groups["width"].Value), int.Parse(match.Groups["height"].Value)));
+                 yield return (int.Parse(match.Groups["width"].Value), int.Parse(match.Groups["height"].Value));
+            }
+               
+            }
+           }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+
+    public static IEnumerable<(Uri url, string title)> Urls(string html) => throw new NotImplementedException();
 }
